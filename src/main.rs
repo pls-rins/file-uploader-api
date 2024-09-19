64480d7ec2c33bba184d96bdfd3ca89854285701
Pierre Le Stunff <pls@pierres-mbp.home>
2024-09-19 17:25:09 +0200

    init

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,11 @@
-//! Run with
-//!
-//! ```not_rust
-//! cargo run -p example-stream-to-file
-//! ```
-
 use axum::{
     body::Bytes,
-    extract::{Multipart, Path, Request},
-    http::StatusCode,
-    response::{Html, Redirect},
-    routing::{get, post},
+    extract::{DefaultBodyLimit, Multipart, Path, Request, State},
+    http::{header, StatusCode},
+    response::{IntoResponse, Response},
+    routing::{post},
     BoxError, Router,
-    response::IntoResponse, Json,
+    Json,
 };
 
 use serde_json::json;
@@ -20,6 +14,10 @@ use std::io;
 use tokio::{fs::File, io::BufWriter};
 use tokio_util::io::StreamReader;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+use aws_sdk_s3::primitives::ByteStream;
+use serde::Serialize;
+use tower_http::limit::RequestBodyLimitLayer;
 
 #[tokio::main]
 async fn main() {
@@ -31,8 +29,15 @@ async fn main() {
         .with(tracing_subscriber::fmt::layer())
         .init();
 
+    let config = aws_config::load_from_env().await;
+    let s3_client = aws_sdk_s3::Client::new(&config);
+
     let app = Router::new()
-        .route("/file/:file_name", post(save_request_body));
+        .route("/file/:file_name", post(save_request_body))
+        .route("/upload", post(upload_hander))
+        .layer(DefaultBodyLimit::disable())
+        .layer(RequestBodyLimitLayer::new(10 * 1024 * 1023))
+        .with_state(s3_client);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8080")
         .await
@@ -94,4 +99,75 @@ fn path_is_valid(path: &str) -> bool {
     }
 
     components.count() == 1
+}
+
+#[derive(Serialize)]
+struct S3File {
+  key: String,
+  successful: bool,
+  url: String,
+  file_name: String,
+  content_type: String,
+  #[serde(skip_serializing)]
+  bytes: Bytes,
+}
+
+async fn upload_hander(
+  State(s3_client): State<aws_sdk_s3::Client>,
+  mut multipart: Multipart,
+) -> Result<Response, Response> {
+  let mut files = vec![];
+  let bucket_name = std::env::var("AWS_BUCKET_NAME").unwrap_or_default();
+
+  while let Some(field) = multipart
+    .next_field()
+    .await
+    .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR.into_response())?
+  {
+    if let Some("files") = field.name() {
+      let file_name = field.file_name().unwrap_or_default().to_owned();
+      let content_type = field.file_name().unwrap_or_default().to_owned();
+      let key = uuid::Uuid::new_v4().to_string();
+      let url = format!("https://{bucket_name}.s3.amazonaws.com/{key}");
+
+      let bytes = field
+        .bytes()
+        .await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR.into_response())?;
+
+      files.push(S3File {
+        file_name,
+        content_type,
+        bytes,
+        key,
+        url,
+        successful: false,
+      })
+    }
+  }
+
+  for file in &mut files {
+    let body = ByteStream::from(file.bytes.to_vec());
+
+    let res = s3_client
+      .put_object()
+      .bucket(&bucket_name)
+      .content_type(&file.content_type)
+      .content_length(file.bytes.len() as i64)
+      .key(&file.key)
+      .body(body)
+      .send()
+      .await;
+
+    file.successful = res.is_ok();
+  }
+
+  Ok(
+    (
+      StatusCode::OK,
+      [(header::CONTENT_TYPE, "application/json")],
+      serde_json::json!(files).to_string(),
+    )
+      .into_response(),
+  )
 }
